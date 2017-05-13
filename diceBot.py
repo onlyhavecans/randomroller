f84c106a7a4224ad1e785535e47037855690a85d
David Aronsohn <WagThatTail@Me.com>
2017-05-13 00:43:27 -0700

    Fully working hardware roller

--- diceBot.py
+++ diceBot.py
@@ -21,6 +21,58 @@ prefix = '!'
 client = commands.Bot(command_prefix=prefix, description=description)
 
 
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+
+@client.command(pass_context=True, description="Rolls dice.\nExamples: 100 Roll 100d6\n3d10 Roll three d10")
+@asyncio.coroutine
+async def roll(ctx, user_roll : str):
+    author = ctx.message.author
+    channel = ctx.message.channel
+    r = {"sides": 6, "die": 0}
+
+    if is_int(user_roll):
+        r["die"] = int(user_roll)
+    elif user_roll.find('d') != -1:
+        r["die"], r["sides"] = map(int, user_roll.split('d'))
+
+    for facet in r:
+        if r[facet] > 100:
+            await client.send_message(channel, "Sorry {}, only 100 {} at a time".format(author, facet))
+            r[facet] = 100
+
+    try:
+        d = Dice(r["die"], r["sides"])
+        rolls = d.rolls()
+        await client.send_message(channel, "{} rolled {} and got {} for a total of {}".format(
+            author, d, ", ".join(map(str, rolls)), sum(rolls)
+        ))
+    except ValueError as e:
+        await client.send_message(channel, "Sorry {}, I don't understand !roll {}".format(author, user_roll))
+        await client.send_message(channel, "Error: {}".format(e))
+
+
+@client.command(pass_context=True, description='Deletes all rolls and bot messages')
+@asyncio.coroutine
+async def purge(ctx):
+    channel = ctx.message.channel
+    deleted = await client.purge_from(channel, limit=500, check=message_is_mine_or_a_roll)
+    await client.send_message(channel, 'Deleted {} message(s)'.format(len(deleted)))
+
+
+def is_int(s : str):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+
 def message_is_mine_or_a_roll(message):
     """
     Message checker for the purge command, used in purge_from
@@ -33,22 +85,6 @@ def message_is_mine_or_a_roll(message):
         return True
     else:
         return False
-
-
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
-
-
-@client.command(pass_context=True, description='Deletes all rolls and bot messages')
-@asyncio.coroutine
-async def purge(ctx):
-    channel = ctx.message.channel
-    deleted = await client.purge_from(channel, limit=500, check=message_is_mine_or_a_roll)
-    await client.send_message(channel, 'Deleted {} message(s)'.format(len(deleted)))
 
 
 def load_credentials():
