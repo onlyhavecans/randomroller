7cb700c5b6174d0b812fae3aeb0f2987929bb60f
David Aronsohn <WagThatTail@Me.com>
2018-04-18 19:07:31 -0700

    Add versioneer
    Signed-off-by: David Aronsohn <WagThatTail@Me.com>

--- randomroller/__init__.py
+++ randomroller/__init__.py
@@ -2,3 +2,7 @@
 
 from randomroller import dice
 from randomroller import die
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+import versioneer
 
 setup(
     name='randomroller',
-    version='1.0',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     requires=['pyserial'],
     description='An extra random dice roller',
     author='David Aronsohn',
